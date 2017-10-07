b2f4745ba35b6bf569553984e4f8ae75f13f5472
Travis CI <build-travis@6700.com.ar>
2017-10-07 11:36:13 -0300

    Added select2

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,28 @@
-console.log('\'Allo \'Allo!');
+(function() {
+  $('.js-input').select2({
+    minimumInputLength: 2,
+    tags: [],
+    ajax: {
+        url: '/data/countries.json',
+        dataType: 'json',
+        type: "GET",
+        quietMillis: 50,
+        data: function (term) {
+            return {
+                term: term
+            };
+        },
+        results: function (data) {
+            return {
+                results: $.map(data, function (item) {
+                    return {
+                        text: item.completeName,
+                        slug: item.slug,
+                        id: item.id
+                    }
+                })
+            };
+        }
+    }
+});
+})()
