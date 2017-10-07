9cc70f326449231dc39282e2c60c3a2b3179a530
Travis CI <build-travis@6700.com.ar>
2017-10-07 11:51:00 -0300

    Fixed conflicts

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,7 +1,25 @@
 (function() {
+
+  function addUserPic (opt) {
+  	if (!opt.id) {
+  		return opt.text;
+  	}
+  	var optimage = $(opt.element).data('image');
+  	if(!optimage){
+  		return opt.text;
+  	} else {
+  		var $opt = $(
+  		'<span class="userName"><img src="' + optimage + '" class="userPic" /> ' + $(opt.element).text() + '</span>'
+  		);
+  		return $opt;
+  	}
+  };
+
   $('.js-input').select2({
     minimumInputLength: 2,
     tags: [],
+		templateResult: addUserPic,
+    templateSelection: addUserPic,
     ajax: {
         url: '/data/countries.json',
         dataType: 'json',
@@ -13,6 +31,7 @@
             };
         },
         results: function (data) {
+          debugger;
             return {
                 results: $.map(data, function (item) {
                     return {
