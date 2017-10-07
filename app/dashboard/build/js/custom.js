e402d852b87230035c649a72400299bc1821fc6b
tomi <tomel95@hotmail.com>
2017-10-07 14:03:25 -0300

    mergin

--- app/dashboard/build/js/custom.js
+++ app/dashboard/build/js/custom.js
@@ -2207,13 +2207,13 @@ if (typeof NProgress != 'undefined') {
 				data: {
 				  labels: ["January", "February", "March", "April", "May", "June", "July"],
 				  datasets: [{
-					label: 'PRECIO FINAL',
-					backgroundColor: "#5449d2",
-					data: [10, 30, 40, 28, 92, 50, 45]
+					label: '# of Votes',
+					backgroundColor: "#26B99A",
+					data: [51, 30, 40, 28, 92, 50, 45]
 				  }, {
-					label: 'PRECIO EN GONDOLA',
-					backgroundColor: "#FE825B",
-					data: [20, 56, 60, 36, 120, 80, 70]
+					label: '# of Votes',
+					backgroundColor: "#03586A",
+					data: [41, 56, 25, 48, 72, 34, 12]
 				  }]
 				},
 
